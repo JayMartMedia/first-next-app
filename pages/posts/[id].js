e16f41a983b16376191a5ba7d9a25dd695603e48
JayMartMedia <Jay@JayMartMedia.com>
2022-02-03 15:03:58 -0500

    Formatted date string and added styling to posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 // gets all valid values for id (runs once for all)
 // runs first
@@ -30,13 +33,18 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            {/* set head */}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            {/* display page content */}
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
